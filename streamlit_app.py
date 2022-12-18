04bd8d038f0c4721b086414e2841f92bbd69f4eb
Milo Mejias <120694926+xamiyuji@users.noreply.github.com>
2022-12-18 20:32:59 +1300

    Update streamlit_app.py
    fix variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ def get_fruit_load_list():
 # allow end user to add a fruit on the list    
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('" + my_fruit + "')")
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
         return "Thanks for adding " + new_fruit
 
 # ------- MAIN PROGRAM ------    
