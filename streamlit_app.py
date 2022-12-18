694b8504284d255f2467dbed9488642ea35f4c8e
Milo Mejias <120694926+xamiyuji@users.noreply.github.com>
2022-12-18 20:09:07 +1300

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,17 +25,15 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information")
     else:
-        back_from_function - get_fruityvice_data(fruit_choice)        
+        back_from_function = get_fruityvice_data(fruit_choice)        
         streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
