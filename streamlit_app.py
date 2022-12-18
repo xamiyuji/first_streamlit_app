275a5a54e6de17d87ce7097dfce256cc582ea70f
Milo Mejias <120694926+xamiyuji@users.noreply.github.com>
2022-12-18 20:29:09 +1300

    Update streamlit_app.py
    add close and fix the input

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ def get_fruit_load_list():
 # allow end user to add a fruit on the list    
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        my_cur.execute("insert into fruit_load_list values ('" + my_fruit + "')")
         return "Thanks for adding " + new_fruit
 
 # ------- MAIN PROGRAM ------    
@@ -57,11 +57,13 @@ streamlit.header("The fruit load list contains:")
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
  
